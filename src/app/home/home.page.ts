c6ef1431cb6f900c454a8152e2c763c4b9109b9e
SynCROSS <51585533+SynCROSS@users.noreply.github.com>
2021-01-26 11:45:12 +0900

    Add Home and ngFor loop

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  myPets = [
+    {
+      kind: 'Dog',
+      name: 'Hot Dog',
+      description: "Dog's not Hot",
+      color: '#dc143c',
+    },
+    {
+      kind: 'Cat',
+      name: 'Hell-o-Kitty',
+      description: "Bill's Favorite",
+      color: 'pink',
+    },
+  ];
+
   constructor() {}
+
+  ngOnInit() {
+    console.log(this.myPets);
+  }
 }
